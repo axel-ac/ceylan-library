91d487cb7ed51477fba8ff450b11ff5d7240234d
axel-ac <abdullahceylan.axel@gmail.com>
2023-07-25 22:53:18 +0300

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Logo, Menu, MenuLink, Nav } from './Navbar.style'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <Nav justify="space-between">
+    <Logo to="/">Ceylan Library</Logo>
+    <Menu>
+      <MenuLink to="/">Home</MenuLink>
+      <MenuLink to="/about">About</MenuLink>
+      <MenuLink to="/register">Register</MenuLink>
+      <MenuLink to="/login">Login</MenuLink>
+      <MenuLink to="/login">Logout</MenuLink>
+    </Menu>
+  </Nav>
+};
 
 export default Navbar
