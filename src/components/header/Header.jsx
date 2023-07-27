28c323d42e425ee8e2ec9761b43bfd212c46e6b5
axel-ac <abdullahceylan.axel@gmail.com>
2023-07-27 23:04:59 +0300

    header-home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
-import { HeaderButton, HeaderContainer, HeaderForm, HeaderTitle, SearchInput, SelectBox } from './Header.style'
+import React from "react";
+import {
+  HeaderButton,
+  HeaderContainer,
+  HeaderForm,
+  HeaderTitle,
+  SearchInput,
+  SelectBox,
+} from "./Header.style";
 
-const Header = () => {
+const Header = ({ setQuery, setSelectType }) => {
+  const printType = ["all", "books", "magazines"];
   return (
     <HeaderContainer>
       <HeaderTitle>Books or Magazines</HeaderTitle>
       <HeaderForm>
-        <SearchInput type="text" placeholder='Search...' required/>
-        <SelectBox>
+        <SearchInput
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value)}
+          required
+        />
+        <SelectBox onChange={(e) => setSelectType(e.target.value)}>
+          {printType.map((type) => {
+            return (
+              <option key={type} value={type}>
+                {type}
+              </option>
+            );
+          })}
           <option>All</option>
         </SelectBox>
-        <HeaderButton type='submit'>
-          Search
-        </HeaderButton>
+        <HeaderButton type="submit">Search</HeaderButton>
       </HeaderForm>
-      </HeaderContainer>
-  )
-}
+    </HeaderContainer>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { HomeContainer, HomeImg } from "./Home.style";
 import Header from "../../components/header/Header";
+
 const Home = () => {
+  
+  const [query, setQuery] = useState("")
+  const [selectType, setSelectType] = useState("all")
+
   return (
     <HomeContainer>
-      <Header />
+      <Header setQuery={setQuery} setSelectType={selectType} />
       <HomeImg></HomeImg>
     </HomeContainer>
   );
