7904cc6b2de3c655c5b3682c25926fd595e102cc
axel-ac <abdullahceylan.axel@gmail.com>
2023-07-29 11:41:50 +0300

    detail

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Description, DetailContainer, DetailImg, DetailTitle, InfoPart } from './Detail.style'
+import { useLocation, useParams } from 'react-router-dom'
 
 const Detail = () => {
+
+  const {state} = useLocation()
+  const {id} = useParams()
+  console.log(state)
+  console.log(id)
+
   return (
-    <div>Detail</div>
+   <DetailContainer>
+    <DetailTitle></DetailTitle>
+    <DetailImg></DetailImg>
+    <Description></Description>
+    <InfoPart></InfoPart>
+   </DetailContainer>
   )
 }
 
